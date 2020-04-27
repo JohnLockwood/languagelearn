ff61d749bf4b7966034ed1eb02bdcb6c730b0dd4
John Lockwood <JohnLockwood2999@gmail.com>
2020-04-26 21:10:54 -0400

    Preliminary Bulma testing

--- src/RegistrationForm.svelte
+++ src/RegistrationForm.svelte
@@ -1,45 +1,36 @@
 <style>
-label {text-align: right; font-size:.8em;}
-input, button {
-	font-family: inherit;
-	font-size: inherit;
-	padding: 0.4em;
-	margin: 0 0 0.5em 0;
-	box-sizing: border-box;
-	border: 1px solid #ccc;
-	border-radius: 2px;
-}
-#registration-form {
-    width: 400px;
-    margin:0px auto;
-    padding-top:1em;
-}
+    #registration-form {
+        width: 300px;
+        margin: 0px auto;
+        padding-top: 1em;
+    }
 
-.loginlink {
-    float:right;
-    text-align:right;
-    
-    font-size:.8em;
-}
+    .loginlink {
+        float: right;
+        text-align: right;
 
-#registration-button {
-position:relative;
-bottom:0;    
-}
+        font-size: .8em;
+    }
+
+    .button-section {
+        margin-top:.4em;
+    }
+
 </style>
 
 <script>
-let email = "";
-let password = "";
+    let email = "";
+    let password = "";
 
-function handleEmailChange() {
-    console.log(email);
-}
+    function handleEmailChange() {
+        console.log(email);
+    }
 </script>
 
 <div id="registration-form">
-<h3>Create a New Account</h3> 
-    <div><label>Email:</label><br /><input on:change={handleEmailChange} type="text" bind:value={email} /></div>
-    <div><label>Password:</label><br /><input type="password" bind:value={password} /></div>
-    <div style="height:100px"><button type="button" id="registration-button">Register</button><span class="loginlink">Already Registered?<br /><a href="javascript:void(0)">Log in</a></span></div>
-</div>
+    <div><h1>Create a New Account</h1></div>
+    <label class="label is-small">Email:</label><input class="input is-small" on:change={handleEmailChange} type="text" bind:value={email} />
+    <label class="label is-small">Password:</label><input class="input is-small" type="password" bind:value={password} />
+    <div class="button-section"><button type="button" class="button is-primary">Register</button><span
+            class="loginlink">Already Registered?<br /><a href="javascript:void(0)">Log in</a></span></div>
+    </div>
