538d96d0d344768539128846cf50775eeb637677
John Lockwood <john.lockwood1@hitachivantara.com>
2020-06-08 13:28:53 -0400

    Updates to docker-compose file for postgres.  Get ready for latest change of mind:  python back end

--- api/infra/connect.sh
+++ api/infra/connect.sh
@@ -1,2 +1,2 @@
 # Assumes you've run docker-compose up -d 
-docker exec -it  my_postgres psql -h localhost -U my_postgres1
+docker exec -it  my_postgres psql -h localhost -U "${POSTGRES_USER}" -d "${POSTGRES_DB}"
