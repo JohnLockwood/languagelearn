ca9f0bd8b47176a94dd847151d53536a8c646251
John Lockwood <jlockwood@verticalrelevance.com>
2021-06-02 08:51:16 -0400

    Basic RDS in TF - no VPC yet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+# For more configuration ideas see
+# https://dev.to/rolfstreefkerk/how-to-setup-a-basic-vpc-with-ec2-and-rds-using-terraform-3jij
+# We have RDS but need VPC
+
 terraform {
   required_providers {
     aws = {
@@ -9,7 +13,10 @@ terraform {
 
 variable db_password {
     type = string
-    default = "UNSET"
+}
+
+variable db_user {
+    type = string
 }
 
 # Configure the AWS Provider
@@ -17,6 +24,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-output "variable_dump" {
+output "variable_dump_password" {
   value = var.db_password
+}
+
+output "variable_dump_user" {
+  value = var.db_user
+}
+
+output "endpoint" {
+  value = aws_db_instance.llearn_db.endpoint
+}
+
+resource "aws_db_instance" "llearn_db" {
+  allocated_storage    = 10
+  engine               = "postgres"
+  engine_version       = "13.2"
+  instance_class       = "db.t3.micro"
+  name                 = "llearn_db"
+  username             = var.db_user
+  password             = var.db_password
+  skip_final_snapshot  = true             # For dev only -- should be false later
 }
