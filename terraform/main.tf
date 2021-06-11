68acf657422e933a3e73111fbc306259ae903580
John Lockwood <jlockwood@verticalrelevance.com>
2021-06-11 17:04:21 -0400

    Preliminary EC2 work

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,17 +32,46 @@ output "variable_dump_user" {
   value = var.db_user
 }
 
-output "endpoint" {
-  value = aws_db_instance.llearn_db.endpoint
+
+output "ip_address" {
+  value = aws_instance.curso_en_ingles.public_ip
 }
 
-resource "aws_db_instance" "llearn_db" {
-  allocated_storage    = 10
-  engine               = "postgres"
-  engine_version       = "13.2"
-  instance_class       = "db.t3.micro"
-  name                 = "llearn_db"
-  username             = var.db_user
-  password             = var.db_password
-  skip_final_snapshot  = true             # For dev only -- should be false later
+resource "aws_key_pair" "curso_en_ingles" {
+  key_name   = "NorthernVirginiaMyEC2KeyPairManaged"
+  public_key = "${file("../secret/NorthernVirginiaMyEC2KeyPair.pub")}"
 }
+
+# TODO Create security group in stack.
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = "sg-09595b1cce392f53f"
+  network_interface_id = aws_instance.curso_en_ingles.primary_network_interface_id
+}
+
+resource "aws_instance" "curso_en_ingles" {
+  depends_on =  [
+    aws_key_pair.curso_en_ingles
+  ]
+
+  ami = "ami-0aeeebd8d2ab47354"
+
+  key_name = "${aws_key_pair.curso_en_ingles.key_name}"
+  instance_type = "t2.micro"  
+  tags = {
+    Name = "CursoEnIngles"
+  }
+}
+# output "endpoint" {
+#   value = aws_db_instance.llearn_db.endpoint
+# }
+
+# resource "aws_db_instance" "llearn_db" {
+#   allocated_storage    = 10
+#   engine               = "postgres"
+#   engine_version       = "13.2"
+#   instance_class       = "db.t3.micro"
+#   name                 = "llearn_db"
+#   username             = var.db_user
+#   password             = var.db_password
+#   skip_final_snapshot  = true             # For dev only -- should be false later
+# }
