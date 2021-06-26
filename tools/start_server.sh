d4aca111498fb37ef873c35ebf6e428bfe1597f9
John Lockwood <johnlockwood2999@gmail.com>
2021-06-26 18:21:48 -0400

    Temporary db endpoint

--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,9 +1,8 @@
 export POSTGRES_USER=$(aws ssm get-parameter --name /curso_en_ingles/dev_prod/db_user --with-decrypt | jq ".Parameter.Value")
 export POSTGRES_PASSWORD=$(aws ssm get-parameter --name /curso_en_ingles/dev_prod/db_password --with-decrypt | jq ".Parameter.Value")
 export POSTGRES_DB=llearn_db
-export POSTGRES_HOST=algo
-
-echo "POSTGRES_DB=$POSTGRES_DB, POSTGRES_USER=$POSTGRES_USER"
+export POSTGRES_HOST="terraform-20210626221425423200000001.cvzppx261icn.us-east-1.rds.amazonaws.com"
+echo "POSTGRES_DB=$POSTGRES_DB, POSTGRES_USER=$POSTGRES_USER POSTGRES_HOST=$POSTGRES_HOST"
 
 #   'NAME': os.environ['POSTGRES_DB'],
 #      'USER': os.environ['POSTGRES_USER'],
