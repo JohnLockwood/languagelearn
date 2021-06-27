9f0013942ef34336f2deb730b723c9ba70683477
John Lockwood <johnlockwood2999@gmail.com>
2021-06-27 11:20:46 -0400

    Update start_server

--- tools/start_server.sh
+++ tools/start_server.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 export POSTGRES_USER=$(aws ssm get-parameter --name /curso_en_ingles/dev_prod/db_user --with-decrypt | jq -r ".Parameter.Value")
 export POSTGRES_PASSWORD=$(aws ssm get-parameter --name /curso_en_ingles/dev_prod/db_password --with-decrypt | jq -r ".Parameter.Value")
 export POSTGRES_DB=llearn_db
@@ -8,8 +14,8 @@ export POSTGRES_HOST="terraform-20210626221425423200000001.cvzppx261icn.us-east-
 #echo "Starting server (not really) HELLO WORLD"
 cd /app/app
 export DJANGO_SETTINGS_MODULE=django_app.settings
-sudo pip3 install requirements.txt
-sudo python3 -m django migrate
-sudo -E python3 -m django runserver 0.0.0.0:80
+pip3 install requirements.txt
+python3 -m django migrate
+python3 -m django runserver 0.0.0.0:80
 
 
